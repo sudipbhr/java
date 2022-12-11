3c9bfa1c7e40c6e4f55484147ae2a475035b1f51
Sudip <sudipbhandari67@gmail.com>
2022-12-11 22:53:42 +0545

    letters printed in the vowels

--- basics/vowels.java
+++ basics/vowels.java
@@ -2,19 +2,21 @@ import java.util.*;
 
 public class vowels {
   public static void main(String[] args) {
-     vowels myObj = new vowels();
+    vowels myObj = new vowels();
     Scanner sc=new Scanner(System.in);
     System.out.print("Enter string");
     String str=sc.nextLine();
-    System.out.println("You have entered:"+str);
+    System.out.println("You have entered: "+str);
     int c=0;
     for(int i=0; i<str.length(); i++){
       // System.out.println("hello");
       char ch = str.charAt(i);
      if(ch=='a' | ch=='e' | ch=='i' | ch=='o' | ch=='u'){
       c=c+1;
+      System.out.print("The vowel letters are "+ch+", ");
      }
+    System.out.println();
     }
-    System.out.println(c);
+    System.out.println("No. of vowel letter is "+c);
      }
 }
