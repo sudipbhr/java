653a44656fb73f8a68cfbc1e68483aa08a70b647
Sudip <sudipbhandari67@gmail.com>
2022-12-11 22:48:58 +0545

    java program to find the total number of vowels in the string

---
+++ basics/vowels.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class vowels {
+  public static void main(String[] args) {
+     vowels myObj = new vowels();
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter string");
+    String str=sc.nextLine();
+    System.out.println("You have entered:"+str);
+    int c=0;
+    for(int i=0; i<str.length(); i++){
+      // System.out.println("hello");
+      char ch = str.charAt(i);
+     if(ch=='a' | ch=='e' | ch=='i' | ch=='o' | ch=='u'){
+      c=c+1;
+     }
+    }
+    System.out.println(c);
+     }
+}
