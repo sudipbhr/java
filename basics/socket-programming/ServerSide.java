3f272d6d28d9d443189ace8b2dcd36ad17f8ed45
Sudip <sudipbhandari67@gmail.com>
2023-03-25 21:50:40 +0545

    server side programming for socket

--- basics/socket-programming/ServerSide.java
+++ basics/socket-programming/ServerSide.java
@@ -1,4 +1,31 @@
 // server side program to show the socket implementation in java
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
 public class ServerSide {
-    
+    public static void main(String args[]) throws IOException{
+        // registering the service at the port 1254
+        ServerSocket ss = new ServerSocket(1254);
+        
+        // wait and accept the connection request from the client
+        Socket cs = ss.accept();
+
+        // get a communication stream associated with the socket
+        Scanner ins = new Scanner(cs.getInputStream());
+        PrintWriter outs = new PrintWriter(cs.getOutputStream(), true);
+
+        // perform IO operation
+        String s = ins.nextLine();
+        System.out.println("from client: "+s);
+        outs.println("hello client"):
+
+        // close the streams and connections
+
+        outs.close();
+        ins.close();
+        cs.close();
+        ss.close();
+
+    }
 }
